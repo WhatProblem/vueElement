746c63e771e4eed1be33c3c1291bc06c519f135e
WhatProblem <18355350915@163.com>
2018-05-06 18:17:59 +0800

    #issue006:markdown部分功能制作 author：WS date: 2018/5/6

--- src/main.js
+++ src/main.js
@@ -7,10 +7,14 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueClipboards from 'vue-clipboards';
+import VueSimplemde from 'vue-simplemde'
+
+// 自定义图标
 import './theme/icons/iconfont.css'
 
 Vue.use(ElementUI)
 Vue.use(VueClipboards)
+Vue.use(VueSimplemde)
 
 Vue.config.productionTip = false
 
