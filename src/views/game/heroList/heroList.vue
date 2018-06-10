1b5b88c38a13677d9b2a1d0874f9a3373607ec0b
WhatProblem <18355350915@163.com>
2018-06-10 08:42:01 +0800

    #issue020:分页部分处理 author:ws date:2018/6/10

--- src/views/game/heroList/heroList.vue
+++ src/views/game/heroList/heroList.vue
@@ -1,70 +1,32 @@
 <template>
   <div class="heroList">
-    <!--<el-container>
-      <el-header>
-        <el-row>
-          <el-button @click="getData" type="primary">获取所有数据</el-button>
-          <el-button type="primary" @click="getTotals">添加英雄数据</el-button>
-        </el-row>
-      </el-header>
-      <el-container>
-        <el-aside width="200px">
-          <ul>
-            <li v-for="(item, index) in options" :key="item.id" @click="getKindFav(item, index)">{{item.title}}</li>
-          </ul>
-        </el-aside>
-        <el-main>
-          <el-table :data="tableData" style="width: 100%" height="400" size="mini">
-            <el-table-column type="index" label="序号" width="60" align="center">
-            </el-table-column>
-            <el-table-column label="英雄名称" width="140" align="left">
-              <template slot-scope="scope">
-                <i class="el-icon-ws-seeuser"></i>
-                <span style="margin-left: 10px">{{ scope.row.game_role }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="英雄战斗力" width="140" align="center">
-              <template slot-scope="scope">
-                <span style="margin-left: 10px">{{ scope.row.game_power }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="英雄使用场次" width="140" align="center">
-              <template slot-scope="scope">
-                <span style="margin-left: 10px">{{ scope.row.game_total }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="英雄喜欢程度" width="140" align="center">
-              <template slot-scope="scope">
-                <span style="margin-left: 10px">{{ scope.row.game_fav }}</span>
-              </template>
-            </el-table-column>
-
-            <el-table-column label="操作" align="center">
-              <template slot-scope="scope">
-                <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-        </el-main>
-      </el-container>
-    </el-container>-->
     <el-container>
       <el-header>
         <el-row>
           <el-button @click="getData" type="primary">获取所有数据</el-button>
-          <el-button type="primary" @click="getTotals">添加英雄数据</el-button>
+          <el-button type="primary" @click="getKindHeroList">添加英雄数据</el-button>
         </el-row>
       </el-header>
       <el-main>
-        <el-tabs @tab-click="handleTabs" tab-position="left" type="border-card" style="height: 200px;">
+        <el-tabs @tab-click="handleTabs" tab-position="left" type="border-card">
           <el-tab-pane v-for="(item, index) in levelOptions" :label="item.title" :key="item.title">
-            <tab-list v-if="curClick==index" :tabListPops="tabListPops"></tab-list>
+            <tab-list v-if="curClick==index" ref="tabLists" :tabListPops="tabListPops"></tab-list>
           </el-tab-pane>
-          <!--<el-tab-pane label="常用等级 - 2"><tab-list></tab-list></el-tab-pane>
-          <el-tab-pane label="常用等级 - 3"><tab-list></tab-list></el-tab-pane>
-          <el-tab-pane label="常用等级 - 4"><tab-list></tab-list></el-tab-pane>-->
         </el-tabs>
+        <el-row>
+          <el-col :span="2">
+            <div class="totals"><span>总：</span><span>{{totalList}}</span></div>
+          </el-col>
+          <el-col :span="8" :offset="14">
+            <el-pagination 
+            :page-size="10" 
+            :pager-count="5" 
+            @current-change="curPage"
+            layout="prev, pager, next" 
+            :total="totalList">
+            </el-pagination>
+          </el-col>
+        </el-row>
       </el-main>
     </el-container>
   </div>
@@ -86,41 +48,62 @@
           { title: '熟练度 - 4级', level: 4 },
         ],
         curClick: '', // 当前点击下标
-        tabListPops: '', // 表格数据
-        tableData: [],
+        tabListPops: [], // 表格数据
+        tableData: [], // 删除数据操作
+        totalList: null, // 数据总数
       }
     },
     mounted() {
-      this.getData();
+      this.curClick = 0;
+      let param = {
+        user_id: '1',
+        pagesTotal: '10', // 每页总数
+        pagesIndex: '1', // 当前页下标
+      };
+      this.getData(param);
     },
     methods: {
       // 选中tabs触发
       handleTabs(tab, event) {
-        console.log(tab);
         this.curClick = tab.index;
+        if (this.curClick === '0') {
+          let param = {
+            user_id: '1',
+            pagesTotal: '10', // 每页总数
+            pagesIndex: '1', // 当前页下标
+          };
+          this.getData(param);
+        } else {
+          let param = {
+            game_fav: this.curClick - 1,
+            user_id: '1',
+            pagesTotal: '10', // 每页总数
+            pagesIndex: '1', // 当前页下标
+          };
+          this.getKindHeroList(param);
+        }
       },
-      getTotals() {
+      getKindHeroList(param) {
         let self = this;
-        let param = {
-          game_fav: '3',
-          user_id: '1',
-          pagesTotal: '10', // 每页总数
-          pagesIndex: '1', // 当前页下标
-        };
+        this.$refs.tabLists[0].loading = true;
         this.$wsApi.get('getDataAndTotals', param, (res) => {
           if (res['data']['code'] === 200) {
-            console.log(res);
+            self.tabListPops = res['data']['data']['data'];
+            self.$refs.tabLists[0].loading = false;
+            self.totalList = res['data']['data']['totals'];
           }
         });
       },
 
       // 获取所有数据
-      getData() {
+      getData(param) {
         let self = this;
-        this.$wsApi.get('allGameRoleList', { user_id: 1 }, (res) => {
+        this.$refs.tabLists[0].loading = true;
+        this.$wsApi.get('allGameRoleList', param, (res) => {
           if (res['data']['code'] === 200) {
             self.tabListPops = res['data']['data']['data'];
-            console.log(self.tabListPops);
+            self.$refs.tabLists[0].loading = false;
+            self.totalList = res['data']['data']['totals'];
           }
         });
       },
@@ -175,44 +158,74 @@
         } else {
           this.getData();
         }
+      },
+      /**
+       * @description: 分页操作
+       */
+      curPage(val) {
+        console.log(val);
+        if (this.curClick === '0') {
+          let param = {
+            user_id: '1',
+            pagesTotal: '10', // 每页总数
+            pagesIndex: val, // 当前页下标
+          };
+          this.getData(param);
+        } else {
+          let param = {
+            game_fav: this.curClick - 1,
+            user_id: '1',
+            pagesTotal: '10', // 每页总数
+            pagesIndex: val, // 当前页下标
+          };
+          this.getKindHeroList(param);
+        }
       }
     }
   }
 
 </script>
 <style lang="scss" scoped>
-  .el-header,
-  .el-footer {
+  .el-header {
     background-color: #B3C0D1;
     color: #333;
     text-align: center;
-    line-height: 60px;
+    line-height: 45px;
   }
   
   .el-aside {
     background-color: #D3DCE6;
     color: #333;
     text-align: center;
-    line-height: 200px;
+    /*line-height: 200px;*/
   }
   
   .el-main {
-    background-color: #E9EEF3;
+    background-color: #fff;
     color: #333;
     text-align: center;
-    line-height: 160px;
+    padding-left: 0px;
+    padding-right: 0px;
+    /*line-height: 160px;*/
+  }
+  
+  .el-tabs {
+    height: 100%;
+  }
+  
+  .el-pagination {
+    text-align: right;
   }
   
   body>.el-container {
-    margin-bottom: 40px;
+    margin-bottom: 20px;
   }
-  
-  .el-container:nth-child(5) .el-aside,
+  /*.el-container:nth-child(5) .el-aside,
   .el-container:nth-child(6) .el-aside {
     line-height: 260px;
   }
   
   .el-container:nth-child(7) .el-aside {
     line-height: 320px;
-  }
+  }*/
 </style>
